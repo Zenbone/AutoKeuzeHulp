b20c41a74472380f250539055729623a511fd374
Zenboen <roshanamanh@outlook.com>
2025-04-30 18:41:13 +0200

    datastructuur verbeterd

--- src/server.js
+++ src/server.js
@@ -1,38 +1,27 @@
-import express from 'express'
-const app = express()
-const port = process.env.PORT || 8080
-app.use(express.json())
+const express = require('express');
+const app = express();
+const port = process.env.PORT || 8080;
+app.use(express.json());
 app.listen(port, () => console.log(`Server gestart op port: ${port}`));
 
-const automerken = {
-    "namen":[{
-        "naam": "toyota", 
-        "jaar": 2024
-    }]
-}
-const automodellen = {
-    "toyota":[{
-        "naam": "corolla",
-        "jaar": 2021
-    }]
-}
-const automodel = {
-    "naam": "corolla",
-    "merk": "toyota",
-    "jaar": 2024
+const autos = {
+    "toyota": {
+      "corolla": {
+        "uitvoering": "hybride",
+        "carrosserie": "hatchback"
+      },
+      "yaris": {
+        "uitvoering": "elektrisch",
+        "carrosserie": "compact"
+      }
+    }
 }
 
-app.get('/data/automerken', (req, res) =>{
-    res.status(200).send(automerken)
-})
-app.get('/data/automodellen', (req, res) =>{
-    res.status(200).send(automodellen)
-})
-app.get('/data/autmodel', (req, res) =>{
-    res.status(200).send(automodel)
+app.get('/autos', (req, res) =>{
+    res.status(200).send(autos)
 })
 
-app.post('/data/automerken', (req, res) =>{
+app.post('/autos', (req, res) =>{
     automerken.push(req.body)
     res.sendStatus(200)
 })
