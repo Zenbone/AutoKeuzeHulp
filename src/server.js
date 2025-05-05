2ec75f7872e5651aa44156fa289ee8bd08af225f
Zenboen <roshanamanh@outlook.com>
2025-05-05 15:33:06 +0200

    .

--- src/server.js
+++ src/server.js
@@ -3,9 +3,7 @@ import cors from 'cors'
 const app = express();
 const port = process.env.PORT || 8080;
 app.use(express.json());
-app.use(cors({
-  origin: 'https://zenbone.site'
-}));
+app.use(cors);
 app.listen(port, () => console.log(`Server gestart op port: ${port}`));
 
 const autos = {
