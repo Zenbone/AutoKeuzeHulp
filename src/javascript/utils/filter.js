6199792a09f36865e066aef3850ce9e9cdfc9187
Zenboen <roshanamanh@outlook.com>
2025-05-21 19:20:04 +0200

    .

--- src/javascript/utils/filter.js
+++ src/javascript/utils/filter.js
@@ -1,12 +1,28 @@
-import { autos } from "../../server"
-
-function pasfiltertoe(filters){
-    // const resultaat = ""
-    // if(filters.uitvoering === "hybride"){
-    //     resultaat.push(autos.toyota.corolla.carrosserie["hybride"])      
-    // }
-    // return resultaat
-    return autos
+function filterAutosMetKeuzes(autos, keuzes){
+	let match = 0;
+	for(let keuze in keuzes){
+		for(let merk in autos){
+			for(let model in merk){
+				for(let spec in model){
+// 					als key bestaat / hetzelfde is als andere key:
+					if(keuze === spec){ 
+// 						als spec een array is bijvoorbeeld meerdere favoriete merken check of alle keuzes
+						if(Array.isArray(keuzes[keuze])){
+							for(let k of keuzes[keuze]){
+								if(keuzes[keuze] === model[spec]){
+									match ++
+								}
+							}
+// 						als spec gewoon string is en value van key hetzelfde is
+						}else if(keuzes[keuze] === model[spec]){
+							match++
+						}
+					}
+				}
+				
+			}
+		}
+	}
 }
 
-export {pasfiltertoe}
+export {filterAutosMetKeuzes}
