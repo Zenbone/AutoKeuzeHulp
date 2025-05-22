d9a4471c5f64bd317592710587ed8f62cb7a6a6a
Zenboen <roshanamanh@outlook.com>
2025-05-22 21:37:31 +0200

    algoritme verbeterd

--- src/javascript/utils/filter.js
+++ src/javascript/utils/filter.js
@@ -1,54 +1,78 @@
 function filterAutosMetKeuzes(autos, keuzes) {
 	let gefilterd = {}
-	let obj = {}
+	let matchpunten = 0
 
 	for (let keuze in keuzes) {
-		for (let merk in autos) {
-			for (let onfav in keuzes.onfavmerken) {
-				if (onfav !== merk || keuzes.onfavmerken === "geen") {
-					// 					merk is niet onfavoriet ga verder met code						
+		if (keuzes[keuze] !== "geen") {
+			for (let merk in autos) {
+				for (let onfav in keuzes.onfavmerken) {
+					if (onfav !== merk) {	// || keuzes.onfavmerken === "geen"
+						for (let model in autos[merk]) {
 
-
-
-
-
-					for (let model in autos[merk]) {
-						for (let spec in autos[merk][model]) {
-
-							if (keuze === spec) {
-								if (Array.isArray(keuzes[keuze])) {
-									console.log("isarray")
-									for (let k in keuzes[keuze]) {
-										// 								check wat voor keuze het is en vergelijk op de juiste manier
-										console.log(k)
-										console.log(keuze)
-
-
-									}
+							matchpunten = 0
+							for (let fav in keuzes.favmerken) {
+								if (fav === merk) {
+									matchpunten++
 								}
-								if (keuzes[keuze] !== "geen") {
-									// 							check wat voor keuze het is en vergelijk op de juiste manier
-									if (keuze === "")
+							}
 
+							for (let specs in autos[merk][model]) {
 
-										if (!gefilterd[merk]) {
-											gefilterd[merk] = {}
+								if (keuzes.minprijs > auto[merk][model].prijs && keuzes.maxprijs < auto[merk][model].prijs) {
+									matchpunten++
+								}
+
+								for (let tag in autos[merk][model][specs]) {
+									if (Array.isArray(keuzes[keuze])) {
+										for (let k in keuzes[keuze]) {
+											if (keuze === "uitvoering") {
+												console.log(k)
+												console.log(autos[merk][model].uitvoering)
+												console.log(autos[merk][model].versnelling)
+												if (k === autos[merk][model].uitvoering) {
+													matchpunten++
+												}
+												if (k === autos[merk][model].versnelling) {
+													matchpunten++
+												}
+											}
+											else if (keuze === "gebruik") {
+												if (k === autos[merk][model][specs][tag]) {
+													matchpunten++
+													// 											break?
+												}
+											}
 										}
-									gefilterd[merk][model] = autos[merk][model]
-									// 							break?
+									} else if (keuze === "interesses") {
+										// 									split string zodat je bv effiecient en goedkoop kan vergelijken met tags van automodel
+										// 									if(splitstringresult === autos[merk][model][spec][tag]){
+										// 										matchpunten++
+										// 										break?
+										// 									}										 
+									} else if (keuze === spec) {
+										if (keuzes[keuze] === autos[merk][model][specs]) {
+											matchpunten++
+											// 										break? ws niet
+										}
+									}
 								}
 							}
 						}
-
+						if (!gefilterd[merk]) {
+							gefilterd[merk] = {}
+						}
+						gefilterd[merk][model] = autos[merk][model]
+						Object.assign(gefilterd[merk][model], { matchpunten: matchpunten })
 					}
+					// 				merk is onfavoriet, overslaan of minpunten geven? als minpunten dan if keuze is geen veranderen. voor nu overslaan
 				}
 			}
 		}
 	}
 	console.log(gefilterd)
-	// 	favmerken bovenaan zetten?
+	// 	return gefilterd en testkeuzes?
+	// 	modellen met hoogste matchpunten bovenaan zetten 
 
-	
 }
 
 export { filterAutosMetKeuzes }
