0d46c0c4aaca3f63617dbab3322b18ed91cba753
Zenboen <roshanamanh@outlook.com>
2025-05-23 18:01:27 +0200

    algoritme

--- src/javascript/utils/filter.js
+++ src/javascript/utils/filter.js
@@ -11,6 +11,7 @@ function filterAutosMetKeuzes(autos, keuzes) {
 						for (let model in autos[merk]) {
 
 							matchpunten = 0
+							criteria = []
 							for (let fav in keuzes.favmerken) {
 								if (keuzes.favmerken[fav] === merk) {
 									criteria.push("favoriete merk")
@@ -23,63 +24,62 @@ function filterAutosMetKeuzes(autos, keuzes) {
 								criteria.push("prijs is goed")
 							}
 
-							for (let specs in autos[merk][model]) {
-								for (let tag in autos[merk][model][specs]) {
-									if (Array.isArray(keuzes[keuze])) {
-										for (let k in keuzes[keuze]) {
-
-											if (keuze === "uitvoering") {
-												if (k === autos[merk][model].uitvoering) {
-													criteria.push("uitvoering klopt!")
-													matchpunten++
-												}
-												if (k === autos[merk][model].versnelling) {
-													criteria.push("versnelling klopt!")
-													matchpunten++
-												}
-											}
-											else if (keuze === "gebruik") {
-												console.log(autos[merk][model][specs][tag])
-												if (k === autos[merk][model][specs][tag]) {
-													criteria.push("gebruik klopt!")
-													matchpunten++
-													// 											break?
-												}
-											}
-										}
-									} else if (keuze === "interesses") {
-										let gesplittestring = keuzes[keuze].split("-")
-										for (let string in gesplittestring) {
-											if (string === autos[merk][model][specs][tag]) {
-												console.log("interesse klopt met tags")
-												criteria.push("interesse klopt met tags")
-												matchpunten++
-												// 												break?
-											}
-										}
-
-									} else if (keuze === specs) {
-										if (keuzes[keuze] === autos[merk][model][specs]) {
-											criteria.push("keuze waarde direct vergeleken en klopt")
+							if (keuze === "uitvoering") {
+								if (Array.isArray(keuzes[keuze])) {
+									for (let k of keuzes[keuze]) {
+										if (k === autos[merk][model].uitvoering ||k === autos[merk][model].versnelling ) {
+											console.log("uitvoering klopt!")
+											criteria.push("uitvoering klopt!")
 											matchpunten++
-											// 										break? ws niet
+										}
+									}
+								} else {
+									if (keuzes[keuze] === autos[merk][model].uitvoering) {
+										console.log("uitvoering klopt!")
+										criteria.push("uitvoering klopt!")
+										matchpunten++
+									}
+								}
+							}
+							else if (keuze === "soort") {
+								for (let k of keuzes[keuze]) {
+									if (k === autos[merk][model].soort) {
+										console.log("soort klopt!")
+										criteria.push("soort klopt!")
+										matchpunten++
+									}
+								}
+							}
+							else if (keuze === "gebruik") {
+								for (let k of keuzes[keuze]) {
+									for (let tag of autos[merk][model].tags) {
+									if (k === tag) {
+										console.log("gebruik klopt met tags")
+										criteria.push("gebruik klopt met tags")
+										matchpunten++
+									}
+								}
+								}
+							}
+							else if (keuze === "interesses") {
+								let gesplittestring = keuzes[keuze].split("-")
+								for (let string of gesplittestring) {
+									for (let tag of autos[merk][model].tags) {
+										if (string === tag) {
+											console.log("interesse klopt met tags")
+											criteria.push("interesse klopt met tags")
+											matchpunten++
 										}
 									}
 								}
 							}
-
-
-							console.log(matchpunten)
 							if (matchpunten !== 0) {
 								if (!gefilterd[merk]) {
 									gefilterd[merk] = {}
-									console.log("merk aangemaakt", merk)
 								}
 								gefilterd[merk][model] = autos[merk][model]
 								Object.assign(gefilterd[merk][model], { matchpunten: matchpunten })
 								Object.assign(gefilterd[merk][model], { criteria: criteria })
-								console.log(gefilterd)
-
 							}
 						}
 					}
@@ -91,7 +91,6 @@ function filterAutosMetKeuzes(autos, keuzes) {
 	console.log(gefilterd)
 	// 	return gefilterd en testkeuzes?
 	// 	modellen met hoogste matchpunten bovenaan zetten 
-
 }
 
 export { filterAutosMetKeuzes }
