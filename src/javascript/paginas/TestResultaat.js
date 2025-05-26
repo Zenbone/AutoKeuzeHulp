e4919436255faabfa6ccde52289ea434f22505b0
Zenboen <roshanamanh@outlook.com>
2025-05-26 16:03:39 +0200

    algoritme en testresultaat 95% af

--- src/javascript/paginas/TestResultaat.js
+++ src/javascript/paginas/TestResultaat.js
@@ -9,32 +9,42 @@
 // })
 
 import { getData } from '../pathservices/GetData.js';
-import { filterAutosMetKeuzes } from '../utils/filter.js';
+import { filterAutosMetKeuzes } from '../utils/Filter.js';
 
 async function main() {
   
   try {
-    let autos = await getData("/autos/toyota");
+    let autos = await getData("/autos");
     let testkeuzes = await getData("/testkeuzes")
-    // 	let autosgefilterd = filterAutosMetKeuzes(autos, testkeuzes)
+    let autosgefilterd = filterAutosMetKeuzes(autos, testkeuzes)
+	// 	komt terug als { model : specs}		(merk binnen specs)
+	autosgefilterd = Object.entries(autosgefilterd)
+// 	  maak array in array van object in objecten
+	autosgefilterd = autosgefilterd.sort((a, b) => b[1].matchpunten - a[1].matchpunten)
+// 	  sorteer op basis van de matchpunten van tweede value van array en omgekeerd van hoog naar laag (b - a)
+// 	console.log(autosgefilterd )
+
     let temp = document.querySelector("#modeltemplate")
 
-    for (let auto in autos) { //autosgefilterd
-      let tempclone = temp.content.cloneNode(true);
+    for (let [modelnaam, specs] of autosgefilterd) { 
+// 	  console.log(specs)
+	  let tempclone = temp.content.cloneNode(true);
       let afbeeldingauto = tempclone.querySelector("img");
-      let modelnaam = tempclone.querySelector("h2");
+      let autonaam = tempclone.querySelector("h3");
       let tags = tempclone.querySelector("p");
-	  let afbeeldingpath = autos[auto].afbeelding //autosgefilterd
-      afbeeldingauto.src = `https://autokeuzehulp.onrender.com${afbeeldingpath}` // autosgefilterd
-      modelnaam.textContent  = auto //autosgefilterd
-      tags.textContent = autos[auto].tags //autosgefilterd
+	  let afbeeldingpath = specs.afbeelding 
+      afbeeldingauto.src = `https://autokeuzehulp.onrender.com${afbeeldingpath}` 
+      autonaam.textContent  = specs.merk + " - " + modelnaam
+      tags.textContent = specs.tags
       document.querySelector(".autosrij").appendChild(tempclone);
     }
   } catch (err) {
     console.error(err);
-    // niks laten zien?
+	 let error = document.createElement("p")
+	 error.textContent = "Oeps er gaat iets fout.. probeer het later opnieuw aub "
+	document.querySelector(".autosrij").appendChild(error);
   }
 
 }
 
-document.addEventListener("DOMContentLoaded", main)
+document.addEventListener("DOMContentLoaded", main())
